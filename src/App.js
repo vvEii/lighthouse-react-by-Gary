2d9274d73cde7b3afc87691d52322bab2a4421d5
vvEii <coopershang125@gmail.com>
2020-10-29 19:03:16 -0700

    practice example

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
 import React, { useState, useEffect } from 'react';
-import axios from "axios";
-import Input from "./Input.js";
+import axios from 'axios';
+import Input from './Input.js';
 import './App.css';
 
+const testData = [
+  { objectID: 1, title: 'google', url: 'http://www.google.ca' },
+];
+
 export default function App() {
-  const title = "My Search Page";
+  const title = 'My Search Page';
   const BASEURL = 'https://hn.algolia.com/api/v1/search?query=';
 
   // Application State
-  const [data, setData] = useState({ hits: [] });
-  const [url, setUrl] = useState();
+  const [data, setData] = useState(testData);
+  const [input, setInput] = useState('hello');
+  const [url, setUrl] = useState('');
 
-  // Fetch data whenever url changes
-  useEffect(() => {
-    axios(url).then(result => setData(result.data));
-  }, [url]);
-
-  // Build array of 'hits'
-  const mappedList = data.hits.map(item => (
-    <li key={item.objectID}>
-      <a href={item.url}>{item.title}</a>
+  const list = data.map((item) => (
+    <li key={item.id}>
+      {item.title}
+      <a href={item.url}></a>
     </li>
   ));
+  // Fetch data whenever url changes
+  useEffect(() => {
+    axios
+    .get(url)
+    .then((res) => setData([...res.data.hits]))
+    .catch((err) => console.log(err));
+  }, [url]);
 
-  const searchClick = function (query) {
-    setUrl(BASEURL + query);
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const onClick = () => {
+    setUrl(BASEURL + input);
   };
 
   return (
-    <div className="App">
-      <h3>{title}</h3>
+    <div className='App'>
+      <h4>Search App</h4>
+      <h4>{input}</h4>
+      <input type='text' value={input} onChange={onChange}></input>
+      <button type='button' onClick={onClick}>
+        Search
+      </button>
       <h4>{url}</h4>
-
-      {/* Use our new Text & Button Component */}
-      <Input buttonText="Search" initial="Mercury" onClick={searchClick} />
-
-      <ul>{mappedList}</ul>
+      <ul>{list}</ul>
     </div>
   );
 }
